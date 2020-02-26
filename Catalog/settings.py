56a12bf31cdedd8a48748b398c3e1664ea0d3032
mehdi <heidari_mahdi84@yahoo.com>
2020-02-26 18:15:35 +0330

    change Installed app in settings

--- Catalog/settings.py
+++ Catalog/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'seed',
-
+    'django_tables2',
+    'bootstrap4',
+    'jquery',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
